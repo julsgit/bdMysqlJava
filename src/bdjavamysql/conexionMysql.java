046a0168e4e191b1e5edee9b5b93d13dc459fd2a
jfuentes <jfuentes@PROF-TC3.labs.cbtis94.local>
2023-11-14 14:37:25 -0600

    Actulizar falla

---
+++ src/bdjavamysql/conexionMysql.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bdjavamysql;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import com.mysql.jdbc.Driver;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jfuentes
+ */
+public class conexionMysql {
+
+    Connection cn;
+
+    public Connection conectar()  {
+        
+        String host = "jdbc:mysql://190.1.0.131/";
+        String user = "julyf";
+        String pass = "";
+        String bd = "bdEjemplo";
+        
+        
+        
+        
+
+        try {
+            
+            cn=DriverManager.getConnection(host+bd, user, pass);
+            JOptionPane.showMessageDialog(null,"conexion exitosa");
+            System.out.println("conexion exitosa");
+                 
+       
+        }
+        catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"ERROR!!"+ex.getMessage());
+            System.out.println(ex.getCause()+"ERROR!!!");
+           
+           
+       
+        }
+        return cn;
+
+    }
+
+}
