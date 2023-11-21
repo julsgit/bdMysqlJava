472e4f95696efcf9a3101fbcae4b60cfd73eea46
juls <juls@localhost>
2023-11-21 08:52:39 -0600

    comentarios desde julsgit

--- src/bdjavamysql/BdJavaMysql.java
+++ src/bdjavamysql/BdJavaMysql.java
@@ -15,6 +15,8 @@ public class BdJavaMysql {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //comentarios desde julsgit
     }
     
 }
